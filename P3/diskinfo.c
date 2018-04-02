f812801d56bdde880a2cc68ac45b487281efbe9f
Brendon Earl <bearl@uvic.ca>
2018-04-02 06:41:57 -0700

    start putting back together

--- P3/diskinfo.c
+++ P3/diskinfo.c
@@ -8,4 +8,7 @@ int main()
     FILE *img = readdisk("test.img");
     struct superblock_t superblock = readsuperblock(img);
     printsuperblock(superblock);
+    struct dir_entry_t *direntries = readfat(img, superblock);
+
+    // printfatinfo(*superblock, direntries);
 }
--- P3/readdisk.c
+++ P3/readdisk.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <unistd.h>
-// #include <fcntl.h>  // file control
-#include <errno.h>  // provides thrown errnos
-#include <string.h> // error stringifying
-#include <math.h>   // pow function
+#include <fcntl.h>     // file control open
+#include <sys/stat.h>  // stat struct
+#include <sys/mman.h>  // mem managment
+#include <arpa/inet.h> // byte endian conversion
+#include <string.h>    // error stringifying
+#include <errno.h>     // provides thrown errnos
+#include <math.h>      // pow function
 #include "disk.h"
-#include <arpa/inet.h>
-
-// #include <sys/types.h>
 
 FILE *readdisk(char *diskpath)
 {
@@ -54,7 +53,23 @@ void printsuperblock(struct superblock_t sb)
     printf("FAT blocks: %d\n", htonl(sb.fat_block_count));
     printf("Root directory start: %d\n", htonl(sb.root_dir_start_block));
     printf("Root directory blocks: %d\n\n", htonl(sb.root_dir_block_count));
+}
 
+void printfatinfo(struct superblock_t sb)
+{
+    unsigned long blksize = htons(sb.block_size);
+    unsigned long size = sizeof(struct dir_entry_t);
+    unsigned long fatlen = (htonl(sb.fat_block_count) - htonl(sb.fat_start_block)) * blksize / size;
+
+    unsigned long freeblocks = 0;
+    unsigned long reservedblocks = 0;
+    unsigned long allocatedblocks = 0;
+
+    unsigned long i;
+    for (i = 0; i < fatlen; i++)
+    {
+        // int h = direntries[i].status;
+    }
     printf("FAT information:\n");
     printf("Free Blocks: %d\n", -1);
     printf("Reserved Blocks: %d\n", -1);

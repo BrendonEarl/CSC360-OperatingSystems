15a7fbb39dd2374b7849db29a19657f0c9caa201
Brendon Earl <bearl@uvic.ca>
2018-04-02 01:26:43 -0700

    Add requirements for first part of diskinfo

--- P3/readdisk.c
+++ P3/readdisk.c
@@ -4,7 +4,10 @@
 // #include <fcntl.h>  // file control
 #include <errno.h>  // provides thrown errnos
 #include <string.h> // error stringifying
+#include <math.h>   // pow function
 #include "disk.h"
+#include <arpa/inet.h>
+
 // #include <sys/types.h>
 
 FILE *readdisk(char *diskpath)
@@ -15,12 +18,12 @@ FILE *readdisk(char *diskpath)
     /* Handle failed file open */
     if (img == NULL)
     {
-        printf("\nfopen() Failed with code [%s]\n", strerror(errno));
+        printf("\nfopen() Failed with code [%s]\n\n", strerror(errno));
         return NULL;
     }
     else
     {
-        printf("\nfopen() Successful\n");
+        printf("\nfopen() Successful\n\n");
     }
 
     return img;
@@ -34,25 +37,26 @@ struct superblock_t readsuperblock(FILE *img)
     return superblock;
 }
 
-void printsuperblock(struct superblock_t superblock)
+void printsuperblock(struct superblock_t sb)
 {
     unsigned long i;
 
     printf("Super block information:\n");
-    printf("Block size: %d\n", -1);
-    printf("Block count: %d\n", -1);
-    printf("FAT starts: %d\n", -1);
-    printf("FAT blocks: %d\n", -1);
-    printf("Root directory start: %d\n", -1);
-    printf("Root directory blocks: %d\n", -1);
+    unsigned long id = 0;
+    unsigned long id_size = (sizeof(sb.fs_id) / sizeof(sb.fs_id[0]));
+    for (i = 0; i < id_size; i++)
+    {
+        id += sb.fs_id[i] * pow(2, (id_size - i));
+    }
+    printf("Block size: %d\n", htons(sb.block_size));
+    printf("Block count: %d\n", htonl(sb.file_system_block_count));
+    printf("FAT starts: %d\n", htonl(sb.fat_start_block));
+    printf("FAT blocks: %d\n", htonl(sb.fat_block_count));
+    printf("Root directory start: %d\n", htonl(sb.root_dir_start_block));
+    printf("Root directory blocks: %d\n\n", htonl(sb.root_dir_block_count));
 
-    printf("FAT information:");
+    printf("FAT information:\n");
     printf("Free Blocks: %d\n", -1);
     printf("Reserved Blocks: %d\n", -1);
     printf("Allocated Blocks: %d\n", -1);
-
-    for (i = 0; i < (sizeof(superblock.fs_id) / sizeof(superblock.fs_id[0])); i++)
-    {
-        printf("%d ", superblock.fs_id[i]);
-    }
 }
